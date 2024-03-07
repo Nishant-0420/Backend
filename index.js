66c13f494b4c990e5e729911646f03e8616a1543
NISHANT <thakurnishant0420@gmail.com>
2024-03-07 18:29:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//  importing
+ const express = require('express'); 
+//  intializing
+const app = express()
+const port=5000
+
+app.get('/',(req,res)=>{ res.send('hello response on the way') })
+app.get('/add',(req,res)=>{ res.send('add is working ') })
+// starting server
+app.listen(port, ()=>console.log("server running !!!"));
