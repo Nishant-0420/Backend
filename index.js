84794e225c4f2e14f19373c19cc563eb4bacea03
NISHANT <thakurnishant0420@gmail.com>
2024-03-08 17:35:28 +0530

    server using express

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express()
 const port=5000
 
 app.get('/',(req,res)=>{ res.send('hello response on the way') })
-app.get('/add',(req,res)=>{ res.send('add is working ') })
+app.get('/add',(req,res)=>{ res.send('add is working !! ') })
 // starting server
 app.listen(port, ()=>console.log("server running!!!"));
