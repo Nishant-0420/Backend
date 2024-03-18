1ee15e0a14eb10abb90f97ef99922a37459da291
NISHANT <thakurnishant0420@gmail.com>
2024-03-18 17:55:33 +0530

    giving response to frontend

--- routers/postrouter.js
+++ routers/postrouter.js
@@ -8,12 +8,21 @@ router.post('/add',(req,res)=>{
     .then((result) => {
         res.status(200).json(result);
     }).catch((err) => {
+        console.log(err)
         res.status(500).json(err)
     });
     
 })
 router.get('/getall',(req,res)=>{
-    res.send('post read operation')
+    // give this overall data abhi tak jitna bhi data add hua hai kyuki abhi filter kuch nhi kiya
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result)
+        
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+    });
 })
 router.get('/delete',(req,res)=>{
     res.send('post delete operation')
