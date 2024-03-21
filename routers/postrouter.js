929888906d5f3583fa6292a5315f914cc74d7cfd
NISHANT <thakurnishant0420@gmail.com>
2024-03-21 18:35:46 +0530

    deleting post

--- routers/postrouter.js
+++ routers/postrouter.js
@@ -27,5 +27,14 @@ router.get('/getall',(req,res)=>{
 router.get('/delete',(req,res)=>{
     res.send('post delete operation')
 })
-
+//: denotes url parameter
+router.delete('/delete/:id',(req,res)=>{
+    Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+        res.status(200).json(result)
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+    });
+})
 module.exports=router;
